9152f3eb450de2efb078ee6b462a886957857d5a
yonghyun123 <model9960@naver.com>
2021-11-22 22:46:41 +0900

    hello servlet

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,16 +1,12 @@
-# Servlet
+package hello.servlet.basic;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-
-> 서블릿은 톰캣 같은 웹 어플리케이션 서버를 직접 설치하고, 그 위에 서블릿 코드를 클래스 파일로 빌드해서 올린 다음, 톰캣 서버를 실행하면 된다. 하지만 이 과정은 번거롭다.
->
-> 스프링 부트는 톰캣 서버를 내장하고 있으므로, 설치 첪이 편리하게 서블릿 코드를 실행할 수 있다.
-
- 
-
-
-
-```java
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
@@ -28,10 +24,3 @@ public class HelloServlet extends HttpServlet {
         response.getWriter().write("hello" + username);
     }
 }
-
-```
-
-- @WebServlet 서블릿 애노테이션
-  - name:서블릿 이름
-  - urlPatterns: URL 매핑
-  - 
