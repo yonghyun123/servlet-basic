5e7a04f9f85e95e7cdecc6174cabac149f7ff72b
yonghyun123 <model9960@naver.com>
2021-12-22 22:33:37 +0900

    move directory

--- src/main/java/hello/servlet/web/frontcontoller/v3/ControllerV3.java
+++
@@ -1,9 +0,0 @@
-package hello.servlet.web.frontcontoller.v3;
-
-import hello.servlet.web.frontcontoller.ModelView;
-
-import java.util.Map;
-
-public interface ControllerV3 {
-    ModelView process(Map<String, String> paramMap);
-}
